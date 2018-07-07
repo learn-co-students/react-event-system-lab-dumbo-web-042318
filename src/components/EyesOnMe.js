c0dc7bd4e0106d30e4c80337570a8814b402dd9a
ahjuice <ahjuice@users.noreply.github.com>
2018-07-07 16:51:14 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleFocus = () => {
+      console.log('Good!')
+    }
+
+    this.handleBlur = () => {
+      console.log('Hey! Eyes on me!')
+    }
+  }
+
+  render() {
+    return (
+      <button
+        onFocus={this.handleFocus}
+        onBlur={this.handleBlur}
+      />
+    )
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = () => {
+      console.log('Entering password...');
+    }
+  }
+
+  render() {
+    return (
+      <input type="password" onKeyUp={this.handleChange} />
+    )
+  }
+}
+
+export default Keypad;
